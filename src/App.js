ca572755cbc0d0fb184d5a68ef802c48029f30e3
Ike <ikescode@gmail.com>
2020-06-03 19:19:34 -0500

    persisting timers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import throttle from 'lodash/throttle';
 import logo from './logo.svg';
 import './App.css';
 import { createStore } from 'redux';
@@ -7,8 +8,13 @@ import reducers from './reducers';
 import NewTimer from './components/new-timer';
 import ListTimers from './components/list-timers';
 import { update } from './actions';
+import { loadState, saveState } from './utils'
 
-const store = createStore(reducers);
+const persistedState = loadState()
+const store = createStore(reducers, persistedState)
+store.subscribe(throttle(() => {
+  saveState(store.getState())
+}, 1000));
 
 let lastUpdateTime = Date.now()
 setInterval(() => {
