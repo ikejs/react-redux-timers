4f27c2fee3cf2fff6833570a2eac8b10eba9854a
Ike <ikescode@gmail.com>
2020-06-03 19:06:55 -0500

    style new timer input

--- src/components/new-timer.js
+++ src/components/new-timer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addTimer } from '../actions'
+import './new-timer.css';
 
 class NewTimer extends Component {
 
@@ -12,14 +13,16 @@ class NewTimer extends Component {
 
   render() {
     return (
-      <div>
+      <div className='new-timer'>
         <input
+          className='new-timer__input'
           type='text'
           placeholder="New Timer Name"
           name="name"
           value={this.state.name}
           onChange={(e) => this.setState({ name: e.target.value })}/>
         <button
+          className='new-timer__button'
           onClick={(e) => {
             this.props.addTimer(this.state.name)
           }}>Save</button>
